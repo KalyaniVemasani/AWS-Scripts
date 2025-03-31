4e4af121360dde71b8721f79423e8d342d918218
KalyaniVemasani <TH88417@umbc.edu>
2025-03-31 18:50:48 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,13 +1,8 @@
-provider "aws" {
-  region = "us-east-1"  # Change this if needed
-}
-
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-08b5b3a93ed654d19"  # Your AMI ID
+resource "aws_instance" "example" {
+  ami           = "ami-071226ecf16aa7d96"
   instance_type = "t2.micro"
-  key_name      = "MyWindowsKeyPair"  # Your Key Pair Name
 
   tags = {
-    Name = "myterraforminstance"  # Instance Name
+    Name = "Terraform-Test-Instance"
   }
 }
