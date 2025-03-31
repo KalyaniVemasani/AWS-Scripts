0941da4679dd820d1959a7d09774dc53a0e0a53a
KalyaniVemasani <TH88417@umbc.edu>
2025-03-31 18:52:36 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-resource "aws_instance" "example" {
-  ami           = "ami-071226ecf16aa7d96"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Terraform-Test-Instance"
-  }
+terraform {
+backend "s3" {
+bucket = "myterraforms3bucketforstoring"
+key = "terraform/state.tfstate"
+region = "us-east-1"
+encrypt = true
+dynamodb_table = "terraform-lock"
 }
+ }
