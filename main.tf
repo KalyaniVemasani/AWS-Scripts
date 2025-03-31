f96608ea1c1b1c19830d8347dd8839679c933070
KalyaniVemasani <TH88417@umbc.edu>
2025-03-31 18:53:04 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
-terraform {
-backend "s3" {
-bucket = "myterraforms3bucketforstoring"
-key = "terraform/state.tfstate"
-region = "us-east-1"
-encrypt = true
-dynamodb_table = "terraform-lock"
+variable "instances" {
+type = map
+default = {
+"web1" = "t2.micro"
+"web2" = "t3.micro"
+"web3" = "t2.small"
+}
+}
+resource "aws_instance" "web" {
+for_each = var.instances
+ami = "ami-071226ecf16aa7d96"
+instance_type = each.value
+tags = {
+Name = "${each.key}"
 }
  }
